e35fd0c47d56fd68d18b01a1e5b4d0418c08fbfa
CarolynYQ <carolynyaker@gmail.com>
2023-05-22 13:17:55 -0500

    cambios en main.js

--- src/data.js
+++ src/data.js
@@ -1,9 +1,42 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+//export const example = () => {
+  //return 'example';
+//};
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+//export const anotherExample = () => {
+  //return 'OMG';
+//};
+
+/*export const nameChampion = (nameOfChampion) => {
+  let key = ""
+  data[key]
+  data.filter( function(name)){
+  return data.name === 'Aatrox'
+  
+}
+console.log(Aatrox);*/
+
+/*function filterData(data, condition) {
+  ctx.fillStyle = color;
+  ctx.fillRect();
+}*/
+
+
+
+
+
+
+/*Ejemplo
+square.js — contiene:
+name — una constante que contiene la cadena «cuadrado».
+draw() — dibuja un cuadrado en un lienzo específico, con un tamaño, posición y color especificados. Devuelve un objeto que contiene el tamaño, la posición y el color del cuadrado.
+
+
+export const name = "square";
+
+export function draw(ctx, length, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, length, length);
+
+  return { length, x, y, color };*/
--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import { example } from './data.js';
+import data from './data/lol/lol.js';
+import data from './data.js';
 
-console.log(example, data);
+//console.log(data);
+//console.log(typeof data)
+/* 
+const nameChampion = data.filter(function(champion)){
+    return champion.name === 'Aatrox';
+    
+  }
+  console.log(nameChampion);*/
+
+const formulario = document.getElementById("form")
+formulario.addEventListener("submit", () => {
+  const buscador = document.getElementById("buscador")
+})
+
+
